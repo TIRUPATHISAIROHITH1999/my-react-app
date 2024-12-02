652be378fb256c60e84283295b2f8466968b3d98
REACT-WEB-ROHITH <tsr19991108@gmail.com>
2024-12-02 08:17:54 +0530

    Update app files

--- src/UserInterfaceComponents/FullView/ViewComponents/NavbarComponent.jsx
+++ src/UserInterfaceComponents/FullView/ViewComponents/NavbarComponent.jsx
@@ -8,7 +8,7 @@ function NavbarComponent() {
   return (
     <Navbar bg="primary" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Brand href="#home">Ltts</Navbar.Brand>
         <Nav className="me-auto">
           {/* Use Link to navigate to routes */}
           <Nav.Link as={Link} to="/my-react-app">Home</Nav.Link>
